44690b5e3bcca87f0014c81abff3d1e1c6191a5f
Florian Angermeir <florian.angermeir@tum.de>
2021-02-01 21:22:50 +0100

    Refine tool patterns

--- analyze.py
+++ analyze.py
@@ -1,6 +1,7 @@
 import os
 import re
 import json
+import shutil
 
 from collections import defaultdict
 from dotenv import load_dotenv
@@ -11,6 +12,13 @@ from github.GithubException import RateLimitExceededException
 
 cases = {'Travis': '.travis.yml', 'GitHub': '.github'}
 results_path = './ci_files/'
+
+def prepare_environment():
+    exists = os.path.exists(results_path)
+    if exists:
+        shutil.rmtree(results_path)
+    
+    os.mkdir(results_path)
 
 
 def authenticate(access_token):
@@ -35,16 +43,15 @@ def save_ci_file(content, ci_sys, repo):
                 with open(results_path + gh_content.name, 'w') as outfile:
                     outfile.write(gh_content.decoded_content.decode('utf-8'))
                 ci_files.append(gh_content.decoded_content.decode('utf-8'))
-        except:
-            # Logging
-            pass
+        except Exception as e:
+            print(e)
     elif ci_sys == 'Travis':
         try:
             with open(results_path + content.name, 'w') as outfile:
                 outfile.write(content.decoded_content.decode('utf-8'))
                 ci_files.append(content.decoded_content.decode('utf-8'))
-        except:
-            pass
+        except Exception as e:
+            print(e)
     else:
         pass
     return ci_files
@@ -66,30 +73,31 @@ def crawl(g, project):
 
 
 def load_sec_tools():
-    with open('security_patterns.json', 'r') as infile:
-        sec_tools = json.load(infile)
+    with open('tool_patterns.json', 'r') as infile:
+        tools = json.load(infile)
 
-    return sec_tools
+    return tools
 
 
-def find_sec_tools(ci_files, sec_tools):
+def find_sec_tools(ci_files, tool_list):
     tool_in_project = defaultdict(list)
     for ci_file in ci_files:
         content = ci_file.lower()
-        for sec_act, tools in sec_tools.items():
+        for act, tools in tool_list.items():
             for tool, details in tools.items():
-                sec = False
+                usage = False
                 for pattern in details['Patterns']:
                     if pattern.lower() in content:
-                        sec = True
-                    
-                    if sec:
-                        tool_in_project[sec_act].append(tool)
+                        usage = True
+
+                    if usage:
+                        tool_in_project[act].append(tool)
     return tool_in_project
 
 
 
 def main():
+    prepare_environment()
     # Load Variables from .env file
     dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
     load_dotenv(dotenv_path)
